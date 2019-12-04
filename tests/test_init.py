b73ed4378539ec364a54694e2e83719c6cf54d26
Julian Stier <mail@julian-stier.de>
2019-12-04 15:06:07 +0100

    First test with module loading

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 from cleo import Application
 from cleo import CommandTester
@@ -16,3 +17,39 @@ def test_value_error_on_unknown_module():
     unknown_module_name = 'some_module'
     with pytest.raises(ValueError, match=r".*%s.*" % unknown_module_name):
         command_tester.execute(unknown_module_name)
+
+
+def test_init_empty_module(tmp_path):
+    application = Application()
+    application.add(InitCommand())
+
+    module_name = 'foo'
+    module_file_path = module_name + '.py'
+    save_path = 'foo-config/model.py'
+    content = '''
+import torch
+
+def init(**args):
+    return torch.nn.Conv2d(10, 10, 10)
+
+'''
+    with open(module_file_path, 'a') as the_file:
+        the_file.write(content)
+
+    command = application.find('init')
+    command_tester = CommandTester(command)
+
+    try:
+        command_tester.execute(module_name + ' --save=' + save_path)
+    except:
+        # Clean up temporary module file
+        os.remove(module_file_path)
+
+        os.remove(save_path)
+        os.remove(os.path.join(os.path.dirname(save_path), 'config.json'))
+        os.removedirs(os.path.dirname(save_path))
+
+    os.remove(module_file_path)
+    os.remove(save_path)
+    os.remove(os.path.join(os.path.dirname(save_path), 'config.json'))
+    os.removedirs(os.path.dirname(save_path))
